2dded0185043a6badc53a46d13a1a546d2d38f61
Marian <marianstowerst@gmail.com>
2021-11-30 21:50:35 +0300

    Build solution structure

---
+++ Tests_Generator/TestGeneratorApp/Block/Generator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace TestGeneratorApp.Block
+{
+    public class Generator
+    {
+        public Task Generate(string sourcePath, string[] fileNames, string destPath, int maxGeneratorTasks)
+        {
+            Directory.CreateDirectory(destPath);
+
+            var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
+            var execOptions = new ExecutionDataflowBlockOptions { MaxDegreeOfParallelism = maxGeneratorTasks };
+
+            //Read files data
+            var downloadStringBlock = new TransformBlock<string, string>
+                (
+                async path =>
+                {
+                    using (var reader = new StreamReader(path)) { return await reader.ReadToEndAsync(); }
+                }, 
+                execOptions
+                );
+
+            return downloadStringBlock.Completion;
+        }
+    }
+}
